19e0d715d5cea633b22401bbe1e181fd385e4bc5
发着呆看星 <2862219036@qq.com>
2023-10-16 19:05:35 +0800

    冒泡与选择排序

---
+++ sort/src/BubbleSort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+/**
+ * @author 发着呆看星
+ * @create 2023/10/16
+ * 冒泡排序：不断的交换，选出最大的数，放在最后，循环进行
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1 ,0 ,2 ,-2 ,9 ,4 ,6 ,-6};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr){
+        if (arr.length <= 2){
+            return;
+        }
+        // 循环冒泡
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 冒泡
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                // 不断交换，将最大的数交换到最后
+                if (arr[j] > arr[j+1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr , int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
---
+++ sort/src/SelectSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+/**
+ * @author 发着呆看星
+ * @create 2023/10/16
+ * 选择排序：遍历数组，选择出最小的数，与数组第一个数进行交换，循环往复
+ */
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr = {1 ,0 ,2 ,-2 ,9 ,4 ,6 ,-6};
+        selectSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void selectSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 选择
+            int min = arr[i];
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]){
+                    min = arr[j];
+                    index = j;
+                }
+            }
+            // 交换
+            BubbleSort.swap(arr,i,index);
+        }
+    }
+
+}
