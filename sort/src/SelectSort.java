a9c984888d1c833afeaf36776d72fd86387000ac
发着呆看星 <2862219036@qq.com>
2023-10-16 19:12:57 +0800

    1

--- sort/src/SelectSort.java
+++ sort/src/SelectSort.java
@@ -7,24 +7,24 @@ import java.util.Arrays;
  */
 public class SelectSort {
     public static void main(String[] args) {
-        int[] arr = {1 ,0 ,2 ,-2 ,9 ,4 ,6 ,-6};
+        int[] arr = {1, 0, 2, -2, 9, 4, 6, -6};
         selectSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void selectSort(int[] arr){
+    public static void selectSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             // 选择
             int min = arr[i];
             int index = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (min > arr[j]){
+                if (min > arr[j]) {
                     min = arr[j];
                     index = j;
                 }
             }
             // 交换
-            BubbleSort.swap(arr,i,index);
+            BubbleSort.swap(arr, i, index);
         }
     }
 
